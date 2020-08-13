21cf6e640b632b66b919fc6f27ec93da86ff1a8e
thiagonmiziara <thiagonmiziara@gmail.com>
2020-08-12 22:08:22 -0300

    melhorando nosso codigo js

---
+++ js/principal.js
@@ -0,0 +1,39 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Thiago Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso invalido.")
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3) {
+        console.log("Altura invalida.");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
---
+++ principal.js
@@ -0,0 +1,9 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Thiago Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+
+var peso = tdPeso.textContent;
+
+console.log(peso);
