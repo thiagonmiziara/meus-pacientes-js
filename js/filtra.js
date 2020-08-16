277f4c2ced08621093db3e58ac947547ff02a46a
thiagonmiziara <thiagonmiziara@gmail.com>
2020-08-16 17:28:05 -0300

    add a funcao de filtar na lista de paciente

---
+++ js/filtra.js
@@ -0,0 +1,26 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function() {
+    console.log(this.value);
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            if (nome != this.value) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+
+
+});
